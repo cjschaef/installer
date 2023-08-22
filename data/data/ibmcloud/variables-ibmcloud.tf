ae251815ce04bd9f2114126d1c1d0d3908576380
cjschaef <cjschaef@us.ibm.com>
2023-08-22 12:42:38 -0500

    CORS-2934: IBMCloud: Basic service endpoint override
    Adding basic support to allow overrides of IBM Cloud
    service endpoints. This is a hard requirement for disconnected
    cluster support, to allow use of non-default endpoints for
    IBM Cloud services.

--- data/data/ibmcloud/variables-ibmcloud.tf
+++ data/data/ibmcloud/variables-ibmcloud.tf
@@ -3,9 +3,8 @@
 #######################################
 
 variable "ibmcloud_api_key" {
-  type = string
-  # TODO: Supported on tf 0.14
-  # sensitive   = true
+  type        = string
+  sensitive   = true
   description = "The IAM API key for authenticating with IBM Cloud APIs."
 }
 
@@ -55,6 +54,12 @@ variable "ibmcloud_image_filepath" {
 # Top-level module variables (optional)
 #######################################
 
+variable "ibmcloud_endpoints_json_file" {
+  type        = string
+  description = "JSON file containing IBM Cloud service endpoints"
+  default     = ""
+}
+
 variable "ibmcloud_preexisting_vpc" {
   type        = bool
   description = "Specifies whether an existing VPC should be used or a new one created for installation."
@@ -70,7 +75,7 @@ variable "ibmcloud_vpc_permitted" {
 variable "ibmcloud_vpc" {
   type        = string
   description = "The name of an existing cluster VPC."
-  default     = null
+  default     = ""
 }
 
 variable "ibmcloud_control_plane_subnets" {
@@ -110,11 +115,10 @@ variable "ibmcloud_publish_strategy" {
   type = string
   description = "The cluster publishing strategy, either Internal or External"
   default = "External"
-  # TODO: Supported on tf 0.13
-  # validation {
-  #   condition     = "External" || "Internal"
-  #   error_message = "The ibmcloud_publish_strategy value must be \"External\" or \"Internal\"."
-  # }
+  validation {
+    condition = can(regex("^External$|^Internal$", var.ibmcloud_publish_strategy))
+    error_message = "The ibmcloud_publish_strategy value must be \"External\" or \"Internal\"."
+  }
 }
 
 variable "ibmcloud_network_resource_group_name" {
