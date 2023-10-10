27a4b817439467aa56d42065598b8363a3d1ae6f
cjschaef <cjschaef@us.ibm.com>
2023-10-10 15:47:20 -0500

    DNM: Testing BYOK support for IBM Cloud
    DO NOT MERGE: Testing BYOK support for IBM Cloud machines.

--- pkg/tfvars/ibmcloud/ibmcloud.go
+++ pkg/tfvars/ibmcloud/ibmcloud.go
@@ -22,26 +22,27 @@ type DedicatedHost struct {
 }
 
 type config struct {
-	Auth                     `json:",inline"`
-	Region                   string          `json:"ibmcloud_region,omitempty"`
-	BootstrapInstanceType    string          `json:"ibmcloud_bootstrap_instance_type,omitempty"`
-	CISInstanceCRN           string          `json:"ibmcloud_cis_crn,omitempty"`
-	DNSInstanceID            string          `json:"ibmcloud_dns_id,omitempty"`
-	ExtraTags                []string        `json:"ibmcloud_extra_tags,omitempty"`
-	MasterAvailabilityZones  []string        `json:"ibmcloud_master_availability_zones"`
-	WorkerAvailabilityZones  []string        `json:"ibmcloud_worker_availability_zones"`
-	MasterInstanceType       string          `json:"ibmcloud_master_instance_type,omitempty"`
-	MasterDedicatedHosts     []DedicatedHost `json:"ibmcloud_master_dedicated_hosts,omitempty"`
-	WorkerDedicatedHosts     []DedicatedHost `json:"ibmcloud_worker_dedicated_hosts,omitempty"`
-	PublishStrategy          string          `json:"ibmcloud_publish_strategy,omitempty"`
-	NetworkResourceGroupName string          `json:"ibmcloud_network_resource_group_name,omitempty"`
-	ResourceGroupName        string          `json:"ibmcloud_resource_group_name,omitempty"`
-	ImageFilePath            string          `json:"ibmcloud_image_filepath,omitempty"`
-	PreexistingVPC           bool            `json:"ibmcloud_preexisting_vpc,omitempty"`
-	VPC                      string          `json:"ibmcloud_vpc,omitempty"`
-	VPCPermitted             bool            `json:"ibmcloud_vpc_permitted,omitempty"`
-	ControlPlaneSubnets      []string        `json:"ibmcloud_control_plane_subnets,omitempty"`
-	ComputeSubnets           []string        `json:"ibmcloud_compute_subnets,omitempty"`
+	Auth                      `json:",inline"`
+	Region                    string          `json:"ibmcloud_region,omitempty"`
+	BootstrapInstanceType     string          `json:"ibmcloud_bootstrap_instance_type,omitempty"`
+	CISInstanceCRN            string          `json:"ibmcloud_cis_crn,omitempty"`
+	DNSInstanceID             string          `json:"ibmcloud_dns_id,omitempty"`
+	ExtraTags                 []string        `json:"ibmcloud_extra_tags,omitempty"`
+	MasterAvailabilityZones   []string        `json:"ibmcloud_master_availability_zones"`
+	WorkerAvailabilityZones   []string        `json:"ibmcloud_worker_availability_zones"`
+	MasterInstanceType        string          `json:"ibmcloud_master_instance_type,omitempty"`
+	MasterDedicatedHosts      []DedicatedHost `json:"ibmcloud_master_dedicated_hosts,omitempty"`
+	WorkerDedicatedHosts      []DedicatedHost `json:"ibmcloud_worker_dedicated_hosts,omitempty"`
+	PublishStrategy           string          `json:"ibmcloud_publish_strategy,omitempty"`
+	NetworkResourceGroupName  string          `json:"ibmcloud_network_resource_group_name,omitempty"`
+	ResourceGroupName         string          `json:"ibmcloud_resource_group_name,omitempty"`
+	ImageFilePath             string          `json:"ibmcloud_image_filepath,omitempty"`
+	PreexistingVPC            bool            `json:"ibmcloud_preexisting_vpc,omitempty"`
+	VPC                       string          `json:"ibmcloud_vpc,omitempty"`
+	VPCPermitted              bool            `json:"ibmcloud_vpc_permitted,omitempty"`
+	ControlPlaneBootVolumeKey string          `json:"ibmcloud_control_plane_boot_volume_key"`
+	ControlPlaneSubnets       []string        `json:"ibmcloud_control_plane_subnets,omitempty"`
+	ComputeSubnets            []string        `json:"ibmcloud_compute_subnets,omitempty"`
 }
 
 // TFVarsSources contains the parameters to be converted into Terraform variables
@@ -78,6 +79,12 @@ func TFVars(sources TFVarsSources) ([]byte, error) {
 		workerAvailabilityZones[i] = c.Zone
 	}
 
+	// Collect controlPlane boot volume information, if it exists
+	var controlPlaneBootVolumeKey string
+	if masterConfig.BootVolume != nil {
+		controlPlaneBootVolumeKey = masterConfig.BootVolume.EncryptionKey
+	}
+
 	// Set pre-existing network config
 	var vpc string
 	masterSubnets := make([]string, len(sources.MasterConfigs))
@@ -93,25 +100,26 @@ func TFVars(sources TFVarsSources) ([]byte, error) {
 	}
 
 	cfg := &config{
-		Auth:                     sources.Auth,
-		BootstrapInstanceType:    masterConfig.Profile,
-		CISInstanceCRN:           sources.CISInstanceCRN,
-		DNSInstanceID:            sources.DNSInstanceID,
-		ImageFilePath:            cachedImage,
-		MasterAvailabilityZones:  masterAvailabilityZones,
-		MasterDedicatedHosts:     sources.MasterDedicatedHosts,
-		MasterInstanceType:       masterConfig.Profile,
-		NetworkResourceGroupName: sources.NetworkResourceGroupName,
-		PublishStrategy:          string(sources.PublishStrategy),
-		Region:                   masterConfig.Region,
-		ResourceGroupName:        sources.ResourceGroupName,
-		WorkerAvailabilityZones:  workerAvailabilityZones,
-		WorkerDedicatedHosts:     sources.WorkerDedicatedHosts,
-		PreexistingVPC:           sources.PreexistingVPC,
-		VPC:                      vpc,
-		VPCPermitted:             sources.VPCPermitted,
-		ControlPlaneSubnets:      masterSubnets,
-		ComputeSubnets:           workerSubnets,
+		Auth:                      sources.Auth,
+		BootstrapInstanceType:     masterConfig.Profile,
+		CISInstanceCRN:            sources.CISInstanceCRN,
+		DNSInstanceID:             sources.DNSInstanceID,
+		ImageFilePath:             cachedImage,
+		MasterAvailabilityZones:   masterAvailabilityZones,
+		MasterDedicatedHosts:      sources.MasterDedicatedHosts,
+		MasterInstanceType:        masterConfig.Profile,
+		NetworkResourceGroupName:  sources.NetworkResourceGroupName,
+		PublishStrategy:           string(sources.PublishStrategy),
+		Region:                    masterConfig.Region,
+		ResourceGroupName:         sources.ResourceGroupName,
+		WorkerAvailabilityZones:   workerAvailabilityZones,
+		WorkerDedicatedHosts:      sources.WorkerDedicatedHosts,
+		PreexistingVPC:            sources.PreexistingVPC,
+		VPC:                       vpc,
+		VPCPermitted:              sources.VPCPermitted,
+		ControlPlaneBootVolumeKey: controlPlaneBootVolumeKey,
+		ControlPlaneSubnets:       masterSubnets,
+		ComputeSubnets:            workerSubnets,
 
 		// TODO: IBM: Future support
 		// ExtraTags:               masterConfig.Tags,
