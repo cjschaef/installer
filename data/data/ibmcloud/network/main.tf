4da3f6b8da6ec85eadbe644967a4317273461a9c
cjschaef <cjschaef@us.ibm.com>
2024-05-29 00:10:24 -0500

    HACK: Revert COS instance plan back to standard
    Local testing in staging required I use the lite COS instance
    plan, limitations on my account. Revert back to standard with
    the assumption other accounts have full access.

--- data/data/ibmcloud/network/main.tf
+++ data/data/ibmcloud/network/main.tf
@@ -28,7 +28,7 @@ data "ibm_resource_group" "group" {
 resource "ibm_resource_instance" "cos" {
   name              = "${var.cluster_id}-cos"
   service           = "cloud-object-storage"
-  plan              = "lite"
+  plan              = "standard"
   location          = "global"
   resource_group_id = local.resource_group_id
   tags              = local.tags
